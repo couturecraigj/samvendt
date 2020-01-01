d36a2f34ca4cdc1fb58522422487f3afaa87c326
Craig Couture <couturecraigj@gmail.com>
2020-01-01 17:58:08 -0600

    update player and link to website

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Built from the beautiful Cara theme from LekoArts customized to be more music focused
 
 icons taken from music [svgrepo](https://www.svgrepo.com/vectors/music-symbols/)
+
+Website available here https://eager-roentgen-1afac6.netlify.com/
--- src/components/soundcloud.tsx
+++ src/components/soundcloud.tsx
@@ -3,11 +3,11 @@ import React from "react"
 const SoundcloudPlayer = () => (
   <iframe
     width="100%"
-    height="300"
+    height="450"
     scrolling="no"
-    // frameborder="no"
+    frameborder="no"
     allow="autoplay"
-    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/7342666&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/7342666&color=%236b554a&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
   ></iframe>
 )
 
